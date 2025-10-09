942d20d689f3abd6d13a2156991f76b167a86112
Pratishtha <pratishtha.richu@gmail.com>
2025-10-09 18:58:26 +0000

    routes init, jwt

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,36 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from './config'
+import { middleware } from './middleware'
 
 const app = express()
+app.use(express.json())
+
+//Signup
+app.post('/signup', (req, res) => {
+  //db call
+
+  res.json({
+    userId: '123'
+  })
+  
+})
+
+app.post('/signin', (req, res) => {
+  const userId = 1
+  const token = jwt.sign({userId}, JWT_SECRET)
+
+  res.json({
+    token
+  }) 
+})
+
+app.post('/room', middleware, (req, res) => {
+  //db call
+
+  res.json({
+    roomId: '101'
+  })
+})
 
 app.listen(3001)
